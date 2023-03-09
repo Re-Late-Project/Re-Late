551a416be4266bde419929a14ee52b65dd7ab55b
Ibrahim Usmani <78166163+IbrahimUsmani118@users.noreply.github.com>
2023-03-09 11:00:02 -0500

    Login Page
    Uploaded the login page for Re-Late Project

---
+++ newuser.php
@@ -0,0 +1,66 @@
+<?php
+// Database credentials
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'your_username');
+define('DB_PASSWORD', 'your_password');
+define('DB_NAME', 'userdatarelate');
+
+// Attempt to connect to MySQL database
+$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if($mysqli === false){
+    die("ERROR: Could not connect. " . $mysqli->connect_error);
+}
+
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = "";
+
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+
+        if($stmt = $mysqli->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bind_param("ss", $param_username, $param_password);
+
+            // Set parameters
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                // Redirect user to login page
+                header("location: login.php");
+                } else {
+                    echo "Oops! Something went wrong. Please try again later.";
+                }
+
+            // Close statement
+            $stmt->close();
+        }
+    }
+
+    // Close connection
+    $mysqli->close();
+}
+?>
