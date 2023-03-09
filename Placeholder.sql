b178c36178beea87ef2d16d9bfb5a575a181890c
Ibrahim Usmani <78166163+IbrahimUsmani118@users.noreply.github.com>
2023-03-09 11:08:28 -0500

    Create Placeholder.sql

---
+++ Placeholder.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (id, name, age, major, gender, year, school_department, history, club_involvement, associations) 
+VALUES (1, 'John Doe', 20, 'Computer Science', 'Male', 2, 'School of Engineering', 'Volunteered at local animal shelter for 2 years', 'Member of the chess club and debate team', 'Member of the computer science association');
