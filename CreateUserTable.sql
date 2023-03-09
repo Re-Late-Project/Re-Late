069623b6c8dcd5d69f5c73f6d8c08232bd406000
Ibrahim Usmani <78166163+IbrahimUsmani118@users.noreply.github.com>
2023-03-09 11:07:14 -0500

    Create CreateUserTable.sql

---
+++ CreateUserTable.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+  id INT PRIMARY KEY,
+  name VARCHAR(255),
+  age INT,
+  major VARCHAR(255),
+  gender VARCHAR(10),
+  year INT,
+  school_department VARCHAR(255),
+  history TEXT,
+  club_involvement TEXT,
+  associations TEXT
+);
